1edf646ae082e28d86c12969baab1e1b419a7b20
RuslanHamidov <rhamidov202@gmail.com>
2024-05-05 19:01:59 +0400

    add changes

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 def home_page():
     return render_template("login.html")
 
-@app.route('/reg', strict_slashes=False)
+
+@app.route('/reg', methods=["GET", "POST"], strict_slashes=False)
 def registration():
-    print("Jopa")
+    login = request.form.get("login")
+    password = request.form.get("password")
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
