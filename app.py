c7347c8c24dfeda8cccaedc2ce562c4b1394600f
Skalersaas <125687364+Skalersaas@users.noreply.github.com>
2024-05-05 21:13:46 +0400

    main

--- app.py
+++ app.py
@@ -10,16 +10,27 @@ def home_page():
     if session.get('current_user') is None:
         return render_template("login.html", users = users)
     else:
-        return render_template("fsa.html", user = session['current_user'])
+        return render_template("userpage.html", user = session['current_user'])
 
 
 @app.route('/make_user', methods=["GET", "POST"], strict_slashes=False)
 def registration():
     login = request.form.get("login")
     password = request.form.get("password")
-    create_user(login, password)
-    session['current_user'] = get_user(login)
-    return redirect("/")
+    if create_user(login, password):
+        session['current_user'] = get_user(login)
+        return redirect("/")
+
+@app.route('/logout')
+def logout():
+    session['current_user'] = None
+    return redirect('/')
+
+@app.route('/delete_user')
+def delete_user():
+    deleteUser(session['current_user'][0][1])
+    session['current_user'] = None
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
--- models.py
+++ models.py
@@ -3,9 +3,14 @@ from os import path
 
 ROOT = path.dirname(path.relpath(__file__))
 
+
+
 def create_user(login, password):
     con = sql.connect(path.join(ROOT, 'database.db'))
     cur = con.cursor()
+    cur.execute('select * from users where logins="{login}"')
+    if cur.fetchall() is not None:
+        return False
     cur.execute('insert into users (logins, passwords) values(?, ?)', (login, password))
     con.commit()
     con.close()
@@ -15,6 +20,7 @@ def get_users():
     cur = con.cursor()
     cur.execute('select * from users')
     users = cur.fetchall()
+    con.close()
     return users
 
 def get_user(login):
@@ -22,4 +28,12 @@ def get_user(login):
     cur = con.cursor()
     cur.execute(f'select * from users where logins="{login}"')
     user = cur.fetchall()
+    con.close()
     return user
+
+def deleteUser(login):
+    con = sql.connect(path.join(ROOT, 'database.db'))
+    cur = con.cursor()
+    cur.execute(f'delete from users where logins="{login}"')
+    con.commit()
+    con.close()
